6edb92dd836ecb60c708d1e5fff8a406411c4af1
msoueidatt <msoueidatt@edb9625f-4e0d-4859-8d74-9fd3b1da38cb>
2013-08-08 17:48:04 +0000

    import of Funclustering packages
    git-svn-id: svn://svn.r-forge.r-project.org/svnroot/soueidatt@16 edb9625f-4e0d-4859-8d74-9fd3b1da38cb

---
+++ pkg/src/Funclustering/src/IAlgo/EMAlgo.cpp
@@ -0,0 +1,24 @@
+#include "EMAlgo.h"
+
+EMAlgo::EMAlgo() {
+	// TODO Auto-generated constructor stub
+}
+
+EMAlgo::EMAlgo(IModel* model) :IAlgo(model) {}
+
+
+EMAlgo::~EMAlgo() {
+	// TODO Auto-generated destructor stub
+}
+
+void EMAlgo::run(bool dimIncrease){
+	m_model -> mStep(dimIncrease);
+	m_model -> eStep();
+	m_model -> logliklihood();
+}
+
+void EMAlgo::run(VectorXi dimFixed){
+	m_model -> mStep(dimFixed);
+	m_model -> eStep();
+	m_model -> logliklihood();
+}
